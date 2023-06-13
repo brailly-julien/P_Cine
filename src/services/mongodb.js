0d55d1dd58df373b90c442bbe52bf51b638c813c
eStrawe <ambroise59.linquette@gmail.com>
2023-06-13 15:35:46 +0200

    ajout service mongodb

---
+++ src/services/mongodb.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("Successfully connected to MongoDB");
+});
+
+module.exports = db;
